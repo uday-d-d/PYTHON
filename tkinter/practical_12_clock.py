39ae50cc85ef8c185aa0504ea3f3e64ada12b3f8
uday-d-d <usdaydn@gamil.com>
2025-09-24 00:25:57 +0530

    Date added

--- tkinter/practical_12_clock.py
+++ tkinter/practical_12_clock.py
@@ -9,9 +9,14 @@ root.configure(bg='black')
 clock_label = tk.Label(root, font=('Helvetica', 48), fg='green', bg='black')
 clock_label.pack()
 
+date_label = tk.Label(root, font=('Helvetica', 20), fg='white', bg='black')
+date_label.pack()
+
 def update_time():
     current_time = strftime('%H:%M:%S')
+    current_date = strftime('%A, %d %B %Y')  # Day, Date Month Year
     clock_label.config(text=current_time)
+    date_label.config(text=current_date)     # Update date
     clock_label.after(1000, update_time)
 
 update_time()
