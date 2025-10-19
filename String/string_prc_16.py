a2b8fc4ac703885289ae70aea56512a13bbb56db
uday-d-d <usdaydn@gamil.com>
2025-10-19 22:04:34 +0530

    New whitespace program

--- String/string_prc_16.py
+++ String/string_prc_16.py
@@ -5,7 +5,6 @@ str1="23"
 str2="Hello0319"
 
 #isalpha(), isdigit(), isalnum()
-
 print(f"Result of isalpha() is : {str.isalpha()}")
 print(f"Result of isdigit() is : {str1.isdigit()}")
 print(f"Result of isalnum() is : {str2.isalnum()}")
@@ -18,12 +17,11 @@ b = str3.lower()
 print(f"\nUppercase : {a}")
 print(f"Lowercase : {b}")
 
-#isspace()
-str5 = " "
-c = str5.isspace()
-print(f"\nResult of isspace() is : {c}")
-
-#whitespace
-str6 = " \t\n\r\v\f"
-result = all(ch in string.whitespace for ch in str6)
-print(f"\nAll characters are whitespace: {result}")
+#isspace() and whitespace
+str = "hello python"
+print("String contains only space : ",str)
+x = str.isspace()
+print("String contains only space : ",x)
+str = "\n \t"
+y = all(ch in string.whitespace for ch in str)
+print("String contains only whitespace : ",y, "\n")
